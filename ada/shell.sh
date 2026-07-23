fe045f3c2b4a48388be3f572af057dac2240bc00
Magnus Weinberg <magnus.weinberg@gmail.com>
2026-07-23 23:42:34 +0200

    fix temporary shell for ada

--- ada/shell.sh
+++ ada/shell.sh
@@ -3,11 +3,13 @@
 set -e
 
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REPO_DIR="$(cd "${PROJECT_DIR}/.." && pwd)"
 DOCKER_IMAGE="gcc:14-bookworm"
-CONTAINER_WORKDIR="/app"
 ALR_VERSION="2.1.1"
 ALR_ZIP="alr-${ALR_VERSION}-bin-x86_64-linux.zip"
 ALR_URL="https://github.com/alire-project/alire/releases/download/v${ALR_VERSION}/${ALR_ZIP}"
+HOST_UID="$(id -u)"
+HOST_GID="$(id -g)"
 
 SETUP_COMMAND="
 set -e
@@ -20,18 +22,22 @@ rm -rf /tmp/alr
 unzip -qo /tmp/alr.zip -d /tmp/alr
 ALR_BIN=\"\$(find /tmp/alr -type f -name alr | head -n 1)\"
 install -m 755 \"\$ALR_BIN\" /usr/local/bin/alr
+# Host bind-mount is owned by UID ${HOST_UID}; git refuses 'dubious ownership' as root.
+git config --global --add safe.directory /repo
+git config --global --add safe.directory '*'
 alr --version
+git -C /repo rev-parse --show-toplevel
 exec /bin/bash
 "
 
 docker run --rm \
     -it \
-    -v "${PROJECT_DIR}:${CONTAINER_WORKDIR}" \
-    -v "${PROJECT_DIR}/../docs:/docs:ro" \
-    -v "${PROJECT_DIR}/..:/repo:ro" \
+    -v "${REPO_DIR}:/repo" \
+    -v "${REPO_DIR}/docs:/docs:ro" \
     -e GH_TOKEN \
     -e ALIRE_GITHUB_TOKEN \
-    -w "${CONTAINER_WORKDIR}" \
+    -e HOME=/root \
+    -w /repo/ada \
     --network=host \
     --user root \
     "${DOCKER_IMAGE}" \
